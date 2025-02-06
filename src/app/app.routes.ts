6ef6a909aecdb12a70d05e03c28801f3372b4b65
Eugenerussel <eugenerussel007@gmail.com>
2025-02-06 15:12:10 +0530

    changes done in the routes (set basepath)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { DailyOpsComponent } from './daily-ops/daily-ops.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/dashboard/1', pathMatch: 'full' },  // Default to Dash 1
+  { path: '', redirectTo: 'dashboard/1', pathMatch: 'full' },  // Default to Dash 1
   { path: 'dashboard/:id', component: DailyOpsComponent }, // Route for Dashboard component
 ];
--- src/app/daily-ops/daily-ops.component.ts
+++ src/app/daily-ops/daily-ops.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-daily-ops',
@@ -10,13 +10,17 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 })
 export class DailyOpsComponent {
   activeTab: string='1';  // To keep track of the active tab
+  basePath: string = '';
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute,private router:Router) {}
 
   ngOnInit() {
     // Subscribe to route parameter changes to switch the active tab
     this.route.params.subscribe(params => {
       this.activeTab = params['id'] || '1';  // Get the active tab id from the route (1 or 2)
     });
+     // Detect if running inside host app (host uses "/business-ops/")
+     this.basePath = this.router.url.includes('business-ops') ? '/business-ops' : '';
+  
   }
 }
